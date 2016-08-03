9c478a7d56c67a5c0c5ebd551750d3b470e436b9
Frank Mosser <mosserfp@yahoo.com>
2016-08-02 19:04:41 -0700

    lots of changes to my_cookbook added

---
+++ cookbooks/my_cookbook/metadata.rb
@@ -0,0 +1,13 @@
+name 'my_cookbook'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures my_cookbook'
+long_description 'Installs/Configures my_cookbook'
+version '0.1.0'
+depends 'build-essential'
+depends 'apache2', '>= 1.0.4'
+depends 'chef-client'
+depends 'apt'
+depends 'ntp'
+
